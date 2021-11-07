d40936890078d77f4af08f4b6f19c66246d60f6c
MrEmmirul21 <emmirulmirul@gmail.com>
2021-11-07 18:29:40 +0000

    this is lab3

---
+++ fork3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+void childTask()
+{
+ printf("Assalamu'alaikum, I'm his daughter\n");
+}
+void parentTask()
+{
+ printf("Wa'alaikumussalam and I'm her father\n");
+}
+
+int main()
+{
+ pid_t pid = fork();
+
+ if (pid==0)
+ {
+  childTask();
+  exit(EXIT_SUCCESS);
+ }
+ else if(pid > 0)
+ {
+  wait(NULL);
+  parentTask();
+ }
+ else
+ {
+  printf("Unable to create child process.");
+ }
+ return EXIT_SUCCESS;
+
+ return 0;
+}
